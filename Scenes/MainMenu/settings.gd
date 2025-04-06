ef608eeda33d1d36655114b36613f59eaeb56dfd
Cip0259 <undermasterbrad@gmail.com>
2025-04-06 02:32:14 +0100

    Items + Menu Progress
    Items given hitboxes and menu started.

---
+++ Scenes/MainMenu/settings.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+var original_scale = Vector2.ONE
+var hover_scale = Vector2(1.2, 1.2)
+
+
+func _ready():
+	original_scale = scale
+	self.pivot_offset = get_size() / 2
+	self.connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	self.connect("mouse_exited", Callable(self, "_on_mouse_exited"))
+
+func _on_mouse_entered():
+	scale = hover_scale
+
+func _on_mouse_exited():
+	scale = original_scale
